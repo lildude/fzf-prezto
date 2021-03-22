73502ccd5a357bc520661c17129461c3282d260a
Colin Seymour <colin@symr.io>
2021-03-22 11:44:44 +0000

    Add colours

---
+++ colors.zsh
@@ -0,0 +1,22 @@
+typeset -A fzf_colors
+
+# Defaults
+fzf_colors["dark"]="dark"
+fzf_colors["light"]="light"
+fzf_colors["16"]="16"
+fzf_colors["bw"]="bw"
+
+fzf_colors["Ayu Mirage"]="fg:#cbccc6,bg:#1f2430,hl:#707a8c,fg+:#707a8c,bg+:#191e2a,hl+:#ffcc66,info:#73d0ff,prompt:#707a8c,pointer:#cbccc6,#73d0ff,spinner:#73d0ff,header:#d4bfff"
+fzf_colors["Dracula"]="dark,fg:-1,bg:-1,hl:#5fff87,fg+:-1,bg+:-1,hl+:#ffaf5f,info:#af87ff,prompt:#5fff87,pointer:#ff87d7,marker:#ff87d7,spinner:#ff87d7"
+fzf_colors["Gruvbox Dark"]="fg:#ebdbb2,bg:#282828,hl:#fabd2f,fg+:#ebdbb2,bg+:#3c3836,hl+:#fabd2f,info:#83a598,prompt:#bdae93,spinner:#fabd2f,pointer:#83a598,marker:#fe8019,header:#665c54"
+fzf_colors["Jellybeans"]="fg:188,bg:233,hl:103,fg+:222,bg+:234,hl+:104,info:183,prompt:110,spinner:107,pointer:167,marker:215"
+fzf_colors["JellyX"]="fg:-1,bg:-1,hl:230,fg+:3,bg+:233,hl+:229,info:150,prompt:110,spinner:150,pointer:167,marker:174"
+fzf_colors["Molokai"]="fg:252,bg:233,hl:67,fg+:252,bg+:235,hl+:81,info:144,prompt:161,spinner:135,pointer:135,marker:118"
+fzf_colors["Nord"]="fg:#D8DEE9,bg:#2E3440,hl:#A3BE8C,fg+:#D8DEE9,bg+:#434C5E,hl+:#A3BE8C,pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B"
+fzf_colors["One Dark"]="dark,fg:-1,bg:-1,hl:#98c379,fg+:#fefefe,bg+:#3e4452,hl+:#98c379,info:#61afef,prompt:#61afef,pointer:#e5c07b,marker:#e5c07b,spinner:#61afef"
+fzf_colors["Paper Color"]="fg:#4d4d4c,bg:#eeeeee,hl:#d7005f,fg+:#4d4d4c,bg+:#e8e8e8,hl+:#d7005f,info:#4271ae,prompt:#8959a8,pointer:#d7005f,marker:#4271ae,spinner:#4271ae,header:#4271ae"
+fzf_colors["Red"]="fg:124,bg:16,hl:202,fg+:214,bg+:52,hl+:231,info:52,prompt:196,spinner:208,pointer:196,marker:208"
+fzf_colors["Seoul256 Dusk"]="fg:242,bg:236,hl:65,fg+:15,bg+:239,hl+:108,info:108,prompt:109,spinner:108,pointer:168,marker:168"
+fzf_colors["Seoul256 Night"]="fg:242,bg:233,hl:65,fg+:15,bg+:234,hl+:108,info:108,prompt:109,spinner:108,pointer:168,marker:168"
+fzf_colors["Solarized Dark"]="dark,hl:33,hl+:37,fg+:235,bg+:136,fg+:254,info:254,prompt:37,spinner:108,pointer:235,marker:235"
+fzf_colors["Solarized Light"]="fg:240,bg:230,hl:33,fg+:241,bg+:221,hl+:33,info:33,prompt:33,pointer:166,marker:166,spinner:33"
